da65f9d883d256bb90e10bba024cf4a206ae82fa
Sandra Andrade <145820828+sammy-regina@users.noreply.github.com>
2025-03-27 20:38:33 -0300

    declarando tipos de variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,22 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;//atribuindo outro valor a variavel
+
+        System.out.println(numero);
+
+        //variavel constante, não pode ser alterada
+        final double VALOR_DE_PI = 3.14;
+
+
     }
+
 }
