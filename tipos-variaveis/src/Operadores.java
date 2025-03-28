e70375e575e0abe9642577846e2869c34cf1a28f
Sandra Andrade <145820828+sammy-regina@users.noreply.github.com>
2025-03-27 21:33:31 -0300

    Operadores de atribuição

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,27 @@
+public class Operadores {
+    public static void main(String[] args) {
+        //Concatenação
+       // String nomeCompleto = "LINGUAGEM" + " JAVA";
+
+        //System.out.println(nomeCompleto);
+
+        String concatenacao = "?";
+
+        concatenacao = 1+1+1+"1";
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao ="1"+(1+1+1);
+        System.out.println(concatenacao);
+                
+    }
+
+}
