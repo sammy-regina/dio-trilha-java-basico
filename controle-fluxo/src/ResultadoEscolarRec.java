03f742e8f478fc0b2e7bc42c397d5ab06d505e22
Sandra Andrade <145820828+sammy-regina@users.noreply.github.com>
2025-03-31 20:20:48 -0300

    Create ResultadoEscolarRecuperação

---
+++ controle-fluxo/src/ResultadoEscolarRec.java
@@ -0,0 +1,16 @@
+public class ResultadoEscolarRec {
+    public static void main(String[] args) {
+        int nota = 6;
+
+        if(nota >= 7)
+        System.out.println("Aprovado");
+        
+        else if(nota >= 5 && nota < 7)
+        System.out.println("Prova recuperação");
+
+        else
+        System.out.println("Reprovado");
+
+    }
+
+}
