1e4d8e1f1a6ae22de93ab72a840cc3278e21eced
Sandra Andrade <145820828+sammy-regina@users.noreply.github.com>
2025-03-31 14:44:37 -0300

    Update Conta Terminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,32 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        //criando o objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        //exibir as mensagens para o usuário
+        System.out.println("Por favor, digite o número da conta: ");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Digite o número da agência: ");
+        String agência = scanner.next();
+
+        System.out.println("Digite o seu nome: ");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Digite o seu sobrenome: ");
+        String sobrenomeCliente = scanner.next();
+
+        System.out.println("Digite o saldo da conta (use ponto como separador decimal):  ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Conta criada com sucesso!");
+
+        System.out.print("Olá " + nomeCliente + " " + sobrenomeCliente + ", obrigada por criar uma conta em nosso banco, sua agência é " + agência + ", conta " + numeroConta + " e seu saldo de R$ " + saldo + " ja está disponível para saque.");
+
+        scanner.close();
+
     }
 }
