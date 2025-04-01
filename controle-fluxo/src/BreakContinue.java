e8dd3b71f15666d52aeb556d6b5ff1fd2b99281f
Sandra Andrade <145820828+sammy-regina@users.noreply.github.com>
2025-03-31 23:25:45 -0300

    Create BreakContinue

---
+++ controle-fluxo/src/BreakContinue.java
@@ -0,0 +1,16 @@
+public class BreakContinue {
+    public static void main(String[] args) {
+
+        for(int numero = 1; numero <=5; numero ++){
+            
+            if(numero == 3)// se numero for = a 3 break
+                //break;
+                continue;
+
+            System.out.println(numero);
+
+        }
+
+    }
+
+}
