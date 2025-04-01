b740ed3f991d1bc6f70efaae68d7377505d92889
Sandra Andrade <145820828+sammy-regina@users.noreply.github.com>
2025-03-31 23:11:48 -0300

    Update ForArrayAlunos

--- controle-fluxo/src/ForArrayAlunos.java
+++ controle-fluxo/src/ForArrayAlunos.java
@@ -1,3 +1,13 @@
 public class ForArrayAlunos {
-    
+    public static void main(String[] args) {
+        String alunos[] = { "Felipe", "Jonas", "Julia", "Marcos"};
+
+        /*for (int x = 0; x < alunos.length; x ++){
+            System.out.println("O aluno de índice x = " + x + " é " + alunos[x]);
+        }*/
+
+        for (String aluno : alunos){
+            System.out.println("Nome do aluno é: " + aluno);
+        }
+    }
 }
