20e8cb37994f16dc438e4783b23294669eb682a8
Sandra Andrade <145820828+sammy-regina@users.noreply.github.com>
2025-04-02 23:42:30 -0300

    Update entrando EmContato

--- controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,5 +1,6 @@
 package candidatura;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.Random;
 
 public class ProcessoSeletivo {
 	public static void main(String[] args) {
@@ -9,10 +10,40 @@ public class ProcessoSeletivo {
 		
 		//selecaoCandidatos();
 		
-		imprimirSelecionados();
+		//imprimirSelecionados();
+		
+		String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO"};
+		for(String candidato: candidatos) {
+			entrandoEmContato(candidato);
+		}
 	}
+	//método ligar/entrar em contato
+	static void entrandoEmContato(String candidato) {
+		int tentativasRealizadas = 1;
+		boolean continuarTentando = true;
+		boolean atendeu = false;
+		do {//condição pra continuar ligando
+			atendeu = atender(); //se atender, atendeu encerra
+			continuarTentando = !atendeu;//se for diferente de atender continuar tentando
+			if(continuarTentando)// se continuar tentando
+				tentativasRealizadas ++;//contar a tentativa realizada + 1 
+			else
+				System.out.println("Contato com " + candidato + " realizado com sucesso!");//se atender(atendeu=true)
+		}while(continuarTentando && tentativasRealizadas <3); //enquanto tentativas realizadas for menor que 3 continuar tentando
+		
+		if(atendeu)
+			System.out.println("CONSEGUIMOS CONTATO COM " + candidato + " NA " + tentativasRealizadas + " TENTATIVA.");
+		else 
+			System.out.println("NÃO CONSEGUIMOS CONTATO COM " + candidato + " NUMERO MAXIMO DE TENTATIVAS: " + tentativasRealizadas);
+	}
+	
+	//método auxiliar "Atender"
+	static boolean atender() {
+		return new Random().nextInt(3)== 1;
+	}
+	
 	static void imprimirSelecionados() {
-		String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA", "DANIELA", "JORGE"};
+		String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO"};
 		System.out.println("Imprimindo a lista de candidatos informando o indice do elemento.");
 		
 		for(int indice = 0; indice < candidatos.length; indice ++) {
@@ -20,9 +51,10 @@ public class ProcessoSeletivo {
 		}
 		System.out.println("Forma abreviada de interação for each");
 		for (String candidato: candidatos) {
-			System.out.println("O candidato selecionado foi: " + candidato);
+			System.out.println("O candidato selecionado foi: " + candidatos);
 		}	
 	}
+	
 	static void selecaoCandidatos() {
 		String[] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA", "DANIELA", "JORGE"};
 		
@@ -41,10 +73,12 @@ public class ProcessoSeletivo {
 			candidatosAtual ++;
 		}
 	}
+	
 	// método para gerar valorPretendido aleatório
 	static double valorPretendido() {
 		return ThreadLocalRandom.current().nextDouble(1800, 2200);
 	}
+	
 	static void analisarCandidato(double salarioPretendido) {
 		double salarioBase = 2000.0;
 		if(salarioBase > salarioPretendido) {
