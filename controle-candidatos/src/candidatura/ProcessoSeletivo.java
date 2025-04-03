e2ddf4a170be37ca02e9544053ff5d14b4d8a01c
Sandra Andrade <145820828+sammy-regina@users.noreply.github.com>
2025-04-02 22:19:54 -0300

    Update ProcessoSeletivo

--- controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,10 +1,34 @@
 package candidatura;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
 	public static void main(String[] args) {
-		analisarCandidato(1900.0);
-		analisarCandidato(2200.0);
-		analisarCandidato(2000.0);
+		//analisarCandidato(1900.0);
+		//analisarCandidato(2200.0);
+		//analisarCandidato(2000.0);
+		selecaoCandidatos();
+	}
+	static void selecaoCandidatos() {
+		String[] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA", "DANIELA", "JORGE"};
+		
+		int candidatosSelecionados = 0;
+		int candidatosAtual = 0;
+		double salarioBase = 2000.0;
+		while(candidatosSelecionados < 5 && candidatosAtual < candidatos.length) {
+			String candidato = candidatos[candidatosAtual];
+			double salarioPretendido = valorPretendido();
+			
+			System.out.println("O candidato " + candidato + " solicitou este valor de salário: R$ " + salarioPretendido);
+			if(salarioBase >= salarioPretendido) {
+				System.out.println("O candidato " + candidato + " foi selecionado para a vaga.");
+				candidatosSelecionados++;
+			}
+			candidatosAtual ++;
+		}
+	}
+	// método para gerar valorPretendido aleatório
+	static double valorPretendido() {
+		return ThreadLocalRandom.current().nextDouble(1800, 2200);
 	}
 	static void analisarCandidato(double salarioPretendido) {
 		double salarioBase = 2000.0;
@@ -12,7 +36,7 @@ public class ProcessoSeletivo {
 			System.out.println("LIGAR PARA O CANDIDATO.");
 		}
 		else if(salarioBase == salarioPretendido) {
-			System.out.println("LIGAR PARA O CANDIDATO CON CONTRAPROPOSTA.");
+			System.out.println("LIGAR PARA O CANDIDATO COM CONTRAPROPOSTA.");
 		}
 		else {
 			System.out.println("AGUARDANDO RRESULTADO DOS DEMAIS CANDIDATOS.");
